437bb854c26d65d63e026e31b040de5d9425fd1a
Belen-progrAmando <belenbarrientos.1985@gmail.com>
2023-10-17 18:22:33 +0200

    edicion footer

--- Alquileres.php
+++ Alquileres.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style2.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -28,7 +28,7 @@
 
 
 </head>
-<body>
+<body >
     <header>
         <img class="imagen" src="./img/banner.jpg" ></img>
         <nav class="navbar navbar-expand-lg navbar-light bg-white" >
